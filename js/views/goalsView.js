81264bf0e72a8d44e76300264993f74ee0cb33d4
Emil Lindau <emillindau@gmail.com>
2013-11-21 22:26:38 +0100

    Yeah.. css

--- js/views/goalsView.js
+++ js/views/goalsView.js
@@ -11,6 +11,7 @@ define(["backbone", "mustache", "goalview"], function(Backbone, Mustache, GoalVi
 
 				if(goal.isValid(true)) {
 					var goalView = new GoalView({model: goal});
+					// this.$(".row").append(goalView.render().el);
 					this.$el.append(goalView.render().el);
 				}
 
